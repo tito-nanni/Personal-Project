3c3d12838a58bef7d2710a134ae117ac415e1c1e
Tito Nanni <titonanni4@gmail.com>
2023-11-16 15:03:15 -0700

    hide details

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -19,34 +19,41 @@ const OrdersPage = () => {
     fetchOrders();
   }, []);
 
-  const handleViewDetails = async(orderDetailId) => {
-    try {
-      const response = await axios.get(`api/order-details/${orderDetailId}`);
-      console.log("Fetched data:", response.data)
-      setOrderDetails(response.data);
-      setSelectedOrder(response.data.order_id);
-    } catch (error) {
-      console.error('Error fetching order details', error);
+  const handleViewDetails = async (orderId) => {
+    // if the currently selected orders details are already shown, hide them
+    if (selectedOrder === orderId) {
+      setSelectedOrder(null);
+      setOrderDetails(null); // reset the order details
+    } else {
+      // fetch and display the new orders details
+      try {
+        const response = await axios.get(`api/order-details/${orderId}`);
+        setOrderDetails(response.data);
+        setSelectedOrder(orderId); // set the newly selected order
+      } catch (error) {
+        console.error('Error fetching order details', error);
+      }
     }
   };
 
-  console.log("Selected Order:", selectedOrder)
-  console.log("Order Details:", orderDetails)
 
   return (
     <div>
-      <h1>Your Orders</h1>
-      {orders.map(order => (
-        <div key={order.order_id}>
-          <h2>Order {order.order_id}</h2>
-          <p>Order Date: {new Date(order.order_date).toLocaleDateString()}</p>
-          <button onClick={() => handleViewDetails(order.order_id)}>View Details</button>
-          </div>
-      ))}
+    <h1>Your Orders</h1>
+    {orders.map(order => (
+      <div key={order.order_id}>
+        <h2>Order {order.order_id}</h2>
+        <p>Order Date: {new Date(order.order_date).toLocaleDateString()}</p>
+        <button onClick={() => handleViewDetails(order.order_id)}>
+          {selectedOrder === order.order_id ? 'Hide Details' : 'View Details'}
+        </button>
+      </div>
+    ))}
+
 
       {selectedOrder && orderDetails && (
         <div>
-          <h2>Order Details for Order</h2>
+          <h2>Order Details</h2>
             <div key={orderDetails.order_detail_id}>
               <p>Product: {orderDetails.Product?.name}</p>
               <p>Quantity: {orderDetails.quantity}</p>
