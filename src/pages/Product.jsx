abdfe22bed575a17311c1afabdae4dc3a6024701
Tito Nanni <titonanni4@gmail.com>
2023-11-29 11:28:30 -0700

    cool css

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../store/features/cartSlice";
 import './Product.css'
 
 const Product = ({ product }) => {
     const dispatch = useDispatch();
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const handleAddToCart = () => {
         dispatch(addToCart(product));
+        setButtonClicked(true);
+        setTimeout(() => setButtonClicked(false), 900); //reset after animation
     }
-
     return (
         <div className="product">
-            <img style={{width: '11rem'}}src={product.image_url} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            <button onClick={handleAddToCart}>Add to Cart</button>
+          <img style={{ width: '11rem' }} src={product.image_url} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+          <button onClick={handleAddToCart} className={buttonClicked ? 'button-clicked' : ''}>
+            Add to Cart
+            <span className="plus-sign">+</span>
+          </button>
         </div>
-    )
-}
+      )
+    }
+    
 
 export default Product;
